0877dc9e0364515e00ec9341aa28a950aff93820
Vihrov Art <vihrov.as@gmail.com>
2018-10-22 16:01:08 +0700

    # - Группировка тестов (mocha)

--- test/express.app.test.js
+++ test/express.app.test.js
@@ -2,15 +2,16 @@ var req = require("supertest");
 var assert = require("assert");
 
 var app = require("../app").app;
-
-it("Express - Echo test", function(done){
-    var phrase = "Hello!!!";
-
-    req(app).get(`/echo/${phrase}`).expect(`<h2>Echo:${phrase}</h2>`).end(done);
-});
-
-it("Express - get object test", function(done){
-    req(app).get("/expressTest").expect(function(response){
-            assert.deepEqual(response.body, {test: "test"});
-    }).end(done);
+describe("Express tests...", function(){
+    it("Express - Echo test", function(done){
+        var phrase = "Hello!!!";
+    
+        req(app).get(`/echo/${phrase}`).expect(`<h2>Echo:${phrase}</h2>`).end(done);
+    });
+    
+    it("Express - get object test", function(done){
+        req(app).get("/expressTest").expect(function(response){
+                assert.deepEqual(response.body, {test: "test"});
+        }).end(done);
+    });
 });
--- test/user.test.js
+++ test/user.test.js
@@ -3,50 +3,52 @@ var assert = require("assert");
 
 var testUser
 
-it("Users - Insert user to base", function(){
-    var user = {"name": "someNameTraLaLa", "age": 400};
-
-    var newUser = users.addNewUser({
-        user: user,
-        onFail: function(message){
-            throw new Error(`При создании пользователя произошла ошибка: ${message}`);
-        },
-        onSuccess: function(user){
-            testUser = user;
+describe("User module tests", function(){
+    it("Users - Insert user to base", function(){
+        var user = {"name": "someNameTraLaLa", "age": 400};
+    
+        var newUser = users.addNewUser({
+            user: user,
+            onFail: function(message){
+                throw new Error(`При создании пользователя произошла ошибка: ${message}`);
+            },
+            onSuccess: function(user){
+                testUser = user;
+            }
+        });
+    });
+    
+    it("Users - Get user by id", function(){
+        if(!testUser){
+            throw new Error(`Нет пользователя для тестирования`);
+        }
+    
+        var user_received = users.getUserById(testUser.id);
+    
+        assert.equal(testUser.name, user_received.name);
+    });
+    
+    it("Users - Get all users", function(){
+        var users_received = users.getAllUsers();
+    
+        if(users_received.constructor != Array){
+            throw new Error(`Полученный объект не является массивом`);
+        }
+    
+        if(users_received.legth){
+            throw new Error(`Полученный массив пуст`);
+        }
+    })
+    
+    it("Users - Remove user", function(){
+        if(!testUser){
+            throw new Error(`Нет пользователя для тестирования`);
+        }
+        
+        var result = users.removeUser(testUser.id);
+    
+        if(!result){
+            throw new Error(`Пользоватлеь для удаления не найден`);
         }
     });
-});
-
-it("Users - Get user by id", function(){
-    if(!testUser){
-        throw new Error(`Нет пользователя для тестирования`);
-    }
-
-    var user_received = users.getUserById(testUser.id);
-
-    assert.equal(testUser.name, user_received.name);
-});
-
-it("Users - Get all users", function(){
-    var users_received = users.getAllUsers();
-
-    if(users_received.constructor != Array){
-        throw new Error(`Полученный объект не является массивом`);
-    }
-
-    if(users_received.legth){
-        throw new Error(`Полученный массив пуст`);
-    }
-})
-
-it("Users - Remove user", function(){
-    if(!testUser){
-        throw new Error(`Нет пользователя для тестирования`);
-    }
-    
-    var result = users.removeUser(testUser.id);
-
-    if(!result){
-        throw new Error(`Пользоватлеь для удаления не найден`);
-    }
 });
